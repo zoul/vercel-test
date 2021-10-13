370379eaded0c3649aaa2565e14acc12ba30b8f0
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-10-13 15:29:02 +0200

    Initial import

---
+++ api/hello.ts
@@ -0,0 +1,9 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import fetch from "node-fetch";
+
+export default async (
+  request: VercelRequest,
+  response: VercelResponse
+): Promise<void> => {
+  response.send("🐈");
+};
